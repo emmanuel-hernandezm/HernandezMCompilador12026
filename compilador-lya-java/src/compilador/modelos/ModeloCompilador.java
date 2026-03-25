502e2bf389641837a3c506321143898a2fae7aaa
EmmH125 <157867619+emmanuel-hernandezm@users.noreply.github.com>
2026-03-25 01:21:24 -0600

    feat: implementar deteccion de carcateres no reconocidos y modificar habilitacion de botones "Compilar"
    Se implemento la deteccion de elementos (caracteres) que no pertenecen o estan declarados en el diagrama sintactico, dentro de un archivo de texto
    
    Falta llevar a revision para obtener feedback hacia la entrega

--- compilador-lya-java/src/compilador/modelos/ModeloCompilador.java
+++ compilador-lya-java/src/compilador/modelos/ModeloCompilador.java
@@ -10,6 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ModeloCompilador {
+    private boolean freeWilly = false;
     
     public String leeContenidoArchivo(File archivo) throws IOException {
         StringBuilder contenido = new StringBuilder();
@@ -21,30 +22,53 @@ public class ModeloCompilador {
         }
         return contenido.toString();
     }
+    
+    public boolean hayCaracteresInvalidos(){
+        return freeWilly;
+    }
 
     public String buscaEnTexto(String texto) {
+        freeWilly = false;
+        
         String regex = "([a-zA-Z]\\w*)|(0|[1-9]\\d*)|(\\+|-|\\*|/)|(==|!=|<=|>=|<|>|=)|(;|,|\\.|\\(|\\))|(\\S)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
 
         List<String> tokens = new ArrayList<>();
+        
         String[] etiquetas = {"", "[I] ", "[N] ", "[OA] ", "[OR] ", "[SE] ", "[E] "};
+        
         int cantidadGrupos = matcher.groupCount();
+        int posError = 1;
         
         while (matcher.find()) {
             for (int i = 1; i <= cantidadGrupos; i++) {
                 if (matcher.group(i) != null) {
-                    tokens.add(etiquetas[i] + matcher.group(i));
+                    if(i == 6){
+                        freeWilly = true;
+                        tokens.add("Error en la posicion [" + posError + "] - Caracter no reconocido: " + matcher.group(i));
+                    } else {
+                        tokens.add(etiquetas[i] + matcher.group(i));
+                    }
+                    posError ++;
                     break;
                 }
             }
         }
 
         StringBuilder reporte = new StringBuilder();
-        reporte.append("Total encontrados: ").append(tokens.size()).append("\n\n");
-        
-        for (int i = 0; i < tokens.size(); i++) {
-            reporte.append((i + 1)).append(". ").append(tokens.get(i)).append("\n");
+        if (freeWilly) {
+            reporte.append("No es posible compilar: \n\n");
+            for (String linea : tokens) {
+                if (linea.startsWith("Error")) {
+                    reporte.append(linea).append("\n");
+                }
+            }
+        } else {
+            reporte.append("COMPILACIÓN EXITOSA\n\n");
+            for (int i = 0; i < tokens.size(); i++) {
+                reporte.append((i + 1)).append(". ").append(tokens.get(i)).append("\n");
+            }
         }
         
         return reporte.toString();
